a65ee1bbbe30ae8443731069a355ce6c5009ca8f
Tejas Pradyumna S <tejaspradyumna.ditio@gmail.com>
2022-02-26 02:09:08 -0500

    Title / FavCon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Ditio Sys | Employee</title>
+      </Head>
+      <link rel="icon"
+        href="https://www.ditiosys.com/wp-content/uploads/2020/09/cropped-image-removebg-preview-1-192x192.png"
+        sizes="192x192"></link>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
