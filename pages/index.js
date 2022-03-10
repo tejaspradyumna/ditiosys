a9467d4cc04d941093dc9ef51d04a6083f65fbf3
Tejas Pradyumna S <tejaspradyumna.ditio@gmail.com>
2022-03-10 14:07:20 -0500

    Index page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Index() {
+
+    useEffect(() => {
+        window.location.href = 'https://ditiosys.com/'
+    }, [])
+
+  return (
+    <></>
+  )
+}
